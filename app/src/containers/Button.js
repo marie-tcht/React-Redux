4298b86e90adb29d038fb1e63749b81856ba9aa0
Marie Tcht <marie.tv@hotmail.fr>
2017-09-11 14:53:01 +0200

    Button stateless component

--- app/src/containers/Button.js
+++ app/src/containers/Button.js
@@ -1,15 +1,52 @@
 /*
  * Npm import
  */
+import { connect } from 'react-redux';
+
 
 /*
  * Local import
  */
+import Button from 'src/components/Button';
+import { vote } from 'src/store/reducer';
+
 
 /*
  * Code
  */
+const mapStateToProps = (state, ownProps) => {
+  // On récupère la propriété qu'on a passé à mon Container dans App
+  const { name } = ownProps;
+
+  // On récupère le state qui m'intéresse
+  const count = state[name];
+
+  // On retourne les propriétés à donner au stateless component
+  return {
+    count,
+  };
+};
+/* const mapStateToProps = (state, ownProps) => ({
+ *   count: state[ownProps.name],
+ * });
+ */
+
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  vote: () => {
+    dispatch(vote(ownProps.name));
+  },
+});
+
+
+/*
+ * Container
+ */
+const createContainer = connect(mapStateToProps, mapDispatchToProps);
+const ButtonContainer = createContainer(Button);
+
 
 /*
  * Export default
  */
+export default ButtonContainer;
